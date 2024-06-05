fc14ca64bd26dffa2f349d1af8224d9cdae09ecc
Aseem <Aseempratap2207@gmail.com>
2024-06-06 02:27:42 +0530

    add styles and contents

--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Editor from "./Editor";
-import { TextField, Button } from "@mui/material";
+import { TextField, Button, Typography, Tooltip } from "@mui/material";
 
 const JoinRoom = () => {
   const [roomId, setRoomId] = useState("");
@@ -9,6 +9,9 @@ const JoinRoom = () => {
   const [joined, setJoined] = useState(false);
 
   const joinRoom = () => {
+    if (!userName) {
+      return;
+    }
     if (!roomId) {
       setRoomId(uuidv4());
     }
@@ -18,33 +21,48 @@ const JoinRoom = () => {
   return (
     <div style={{ backgroundColor: "#f1f0f0" }}>
       {!joined ? (
-        <div
-          style={{
-            position: "fixed",
-            left: "50%",
-            top: "50%",
-            transform: "translate(-50%, -50%)",
-            maxWidth: "400px",
-          }}
-        >
-          <form>
+        <div>
+          <Typography variant="h3" sx={{ textAlign: "center" }}>
+            Welcome To Code Collab
+          </Typography>
+          <Typography variant="h6" mt="10px" sx={{ textAlign: "center" }}>
+            {" "}
+            Collaborative Coding Made Easy
+          </Typography>
+          <form
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              position: "fixed",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+              maxWidth: "400px",
+            }}
+          >
             <TextField
               label="Name"
               variant="outlined"
               fullWidth
               margin="normal"
               value={userName}
+              required
               onChange={(e) => setUserName(e.target.value)}
             />
-            <TextField
-              label="Email"
-              placeholder="Enter room ID or leave empty to create a new room"
-              variant="outlined"
-              fullWidth
-              margin="normal"
-              value={roomId}
-              onChange={(e) => setRoomId(e.target.value)}
-            />
+            <Tooltip
+              placement="right-end"
+              title="Enter room ID or leave empty to create a new room"
+            >
+              <TextField
+                label="RoomID"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={roomId}
+                onChange={(e) => setRoomId(e.target.value)}
+              />
+            </Tooltip>
+
             <Button
               variant="contained"
               color="secondary"
